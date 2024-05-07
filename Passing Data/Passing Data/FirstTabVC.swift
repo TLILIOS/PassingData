4e12671db026984b7f5e3d9fc8ed7bb777d22884
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-07 19:48:05 +0200

    Notification

---
+++ Passing Data/Passing Data/FirstTabVC.swift
@@ -0,0 +1,23 @@
+//
+//  FirstTabVC.swift
+//  Passing Data
+//
+//  Created by HTLILI on 07/05/2024.
+//
+
+import UIKit
+
+class FirstTabVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func btnPostNotification(_ sender: UIButton) {
+        
+        
+    }
+    
+}
---
+++ Passing Data/Passing Data/ForthTabVC.swift
@@ -0,0 +1,29 @@
+//
+//  ForthTabVC.swift
+//  Passing Data
+//
+//  Created by HTLILI on 07/05/2024.
+//
+
+import UIKit
+
+class ForthTabVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
